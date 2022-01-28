bee90ede453a344b6d45a672bb7404f336705466
hhandika <herubiolog@gmail.com>
2022-01-28 15:52:50 -0600

    Add timer.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use std::path::Path;
+use std::time::Instant;
 
 mod bytes;
 
 fn main() {
     let files = Path::new("tests/test_files/simple.nex");
+    let fasta = Path::new("tests/test_files/simple.fas");
     bytes::read_nexus_bytes(files).unwrap();
 
-    println!("Fasta:");
-    let fasta = Path::new("tests/test_files/simple.fas");
-    bytes::read_fasta_bytes(fasta).unwrap();
-
     println!("Iterator fasta:");
+    let time_2 = Instant::now();
     bytes::iter_fasta_bytes(fasta).unwrap();
+    println!("Execution time: {:?}", time_2.elapsed());
+
+    println!("Fasta:");
+    let time = Instant::now();
+    bytes::read_fasta_bytes(fasta).unwrap();
+    println!("Execution time: {:?}", time.elapsed());
 }
